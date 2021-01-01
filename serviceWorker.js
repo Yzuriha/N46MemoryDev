6f6be92df3f1083800bf86a50ca21e7d2b68372e
Yzuriha <inoriislove@gmail.com>
2021-01-01 15:31:35 +0100

    fix pwa 3

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,37 +1,31 @@
 const memoryGame = "memoryGame-v1"
 const assets = [
   "/",
+  "N46Memory",
   "manifest.json",
   "index.html",
   "css/app.css",
   "js/app.js",
   "data/data.js",
   "data/assets/NogiText.png",
-  "data/assets/logo.png",
+  "N46Memory/data/assets/logo.png",
   "data/assets/logo.svg",
   "data/assets/logoAnimated.svg",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css",
   "https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.1.0/css/hover-min.css"
 ]
 
-const test = [
-  "data/images/asuka1.jpg"
-]
-
 // in the service worker
 addEventListener('message', event => {
   // event is an ExtendableMessageEvent object
-  // console.log(`The client sent me a message: ${event.data}`);
   var data = JSON.parse(event.data);
   var cardSet = [];
 
-  // if(navigator.online){
   data.forEach(item => {
     item.img.forEach(img => {
       cardSet.push(`data/images/${img}`)
     })
   })
-// }
 
   caches.open(memoryGame).then(cache => {
     cardSet.forEach((item, i) => {
