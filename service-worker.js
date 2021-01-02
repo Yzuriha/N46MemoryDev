e110d668fbdf4b6e1cbca5a9a9fbd75be100f3f5
Yzuriha <inoriislove@gmail.com>
2021-01-02 01:06:55 +0100

    fix a bit of css

--- service-worker.js
+++ service-worker.js
@@ -18,8 +18,6 @@ let filesToCache = [
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css",
   "https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.1.0/css/hover-min.css",
   "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-  // add your assets here
-  // ❗️❕donot add config.json here ❗️❕
 ];
 
 
